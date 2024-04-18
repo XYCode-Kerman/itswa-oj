52ca242a86bd2f815bc132d21ed92de36233653c
XYCode Kerman <xycode-xyc@outlook.com>
2024-04-18 13:19:05 +0800

    feat: 新增ITED到页脚

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,15 +1,17 @@
 <template>
     <footer class="footer bg-base-200 p-10 text-base-content grid grid-cols-4">
         <div class="col-span-2">
-            <h6 class="footer-title">ItsWA Online Judge</h6>
+            <h6 class="footer-title">ItsWA Online Judge (ITOJ)</h6>
             <p>开发版本，不代表最终产品质量。</p>
             <p class="text-xs">Development version, does not represent the quality of the final product.</p>
         </div>
 
         <nav>
-            <h6 class="footer-title">联系我们</h6>
+            <h6 class="footer-title">姊妹项目</h6>
             <a href="https://github.com/XYCode-Kerman/ItsWA" class="link link-hover">ItsWA on Github</a>
-            <a href="https://github.com/XYCode-Kerman/ItsWA-OJ" class="link link-hover">ItsWA-OJ on Github</a>
+            <a href="https://github.com/XYCode-Kerman/ItsWA-Editor" class="link link-hover">ItsWA-Editor (ITED) on
+                Github</a>
+            <a href="https://github.com/XYCode-Kerman/ItsWA-OJ" class="link link-hover">ItsWA-OJ (ITOJ) on Github</a>
         </nav>
     </footer>
 </template>
